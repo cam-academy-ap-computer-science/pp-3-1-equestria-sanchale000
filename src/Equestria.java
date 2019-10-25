3b345bd92e6fa4a6574135cc5a1a5749b4c03c5d
sanchale000 <sanchale000@CAM-LAB-18.bgsd.k12.wa.us>
2019-10-25 07:51:39 -0700

    Done?

--- src/Equestria.java
+++ src/Equestria.java
@@ -2,8 +2,15 @@
 public class Equestria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		distance(31, 35, 15, 9);
+		distance(6, 24, 20, 7);
+		distance(28, 16, 22, 13);
+	
+	}
+	public static double distance(double xOne, double xTwo, double yOne, double yTwo) {
+		double d = Math.sqrt(Math.pow(xTwo - xOne, 2) + Math.pow(yTwo - yOne, 2));
+		System.out.println("The distance is " + d);
+				return d;
 	}
 
 }
